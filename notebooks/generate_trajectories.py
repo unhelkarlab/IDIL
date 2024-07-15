f3dc99219761ab619063980bb5a75a5bf187dd77
JuanseHevia <jh216@rice.edu>
2024-07-15 17:41:58 -0500

    start utils for generating trajectories

---
+++ notebooks/generate_trajectories.py
@@ -0,0 +1,11 @@
+from idil_train import vis_cleanup as vis_utils
+import os 
+from collections import defaultdict
+import numpy as np
+import torch 
+import utils
+
+def generate_trajectory(run_id: str):
+    """
+    Load
+    """
