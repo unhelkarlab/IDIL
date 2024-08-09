76df3a0d8b63b68dbfab2eed011daaab824bd4e1
JuanseHevia <jh216@rice.edu>
2024-08-09 12:03:16 -0500

    changed supervision defaults in IDIL bash scripts

--- idil_train/script_examples/idil_run.sh
+++ idil_train/script_examples/idil_run.sh
@@ -46,22 +46,22 @@ python idil_train/run_algs.py alg=idil base=boxpush_base \
 
 ###### MISC
 # Nohup with fixed expert policy (discrete)
-nohup python idil_train/run_algs.py alg=idil base=boxpush_base env=CleanupSingle-v0 seed=0 supervision=0.0 fixed_pi=true  tag='disc-fep'&
+nohup python idil_train/run_algs.py alg=idil base=boxpush_base env=CleanupSingle-v0 seed=0 supervision=0.5 fixed_pi=true  tag='disc-fep'&
 
 # Nohup without fixed expert policy (discrete)
-nohup python idil_train/run_algs.py alg=idil base=boxpush_base env=CleanupSingle-v0 seed=0 supervision=0.0 fixed_pi=false &
+nohup python idil_train/run_algs.py alg=idil base=boxpush_base env=CleanupSingle-v0 seed=0 supervision=0.5 fixed_pi=false &
 
 # Nohup with fixed expert policy (continuous)
-nohup python idil_train/run_algs.py alg=idil base=MultiGoals2D_base env=MultiGoals2D_3-v0 supervision=0.0 seed=0 fixed_pi=true tag='cont-fep' &
+nohup python idil_train/run_algs.py alg=idil base=MultiGoals2D_base env=MultiGoals2D_3-v0 supervision=0.5 seed=0 fixed_pi=true tag='cont-fep' &
 
 
 ##### ENTROPY SCORING 
 # Entropy scorign @ k = 20% (Discrete env)
-nohup python idil_train/run_algs.py alg=idil base=boxpush_base env=CleanupSingle-v0 seed=0 supervision=0.0 k=0.2 entropy_scoring=true tag='disc-es-20'&
+nohup python idil_train/run_algs.py alg=idil base=boxpush_base env=CleanupSingle-v0 seed=0 supervision=1.0 k=0.2 entropy_scoring=true tag='disc-es-20'&
 # Entropy scoring @ k = 60% (Continuous env)
-nohup python idil_train/run_algs.py alg=idil base=MultiGoals2D_base env=MultiGoals2D_3-v0 supervision=0.0 k=0.6 entropy_scoring=true tag='cont-es-60'&
+nohup python idil_train/run_algs.py alg=idil base=MultiGoals2D_base env=MultiGoals2D_3-v0 supervision=1.0 k=0.6 entropy_scoring=true tag='cont-es-60'&
 
 # sweep k-values list
-nohup python entropy_scoring_sweep.py --sweep_k='0.2;0.4;0.6' --num_trials=5&
+nohup python entropy_scoring_sweep.py --sweep_k='0.2;0.4;0.6' --num_trials=5 &
 # sweep k-values list (continuous env)
 nohup python entropy_scoring_sweep.py --sweep_k='0.2;0.4;0.6' --base=MultiGoals2D_base --env=MultiGoals2D_3-v0 --discrete_env=false --num_trials=5 &
