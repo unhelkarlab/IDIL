efff67e9cc1f670a543f39afb26ef139872d80a2
JuanseHevia <jh216@rice.edu>
2024-08-10 19:12:52 -0500

    added fixed_pi support to K val sweeping

--- entropy_scoring_sweep.py
+++ entropy_scoring_sweep.py
@@ -4,7 +4,7 @@ from ulid import ULID
 import numpy as np
 
 COMMAND_STR_TEMPLATE = '''
-python idil_train/run_algs.py alg={alg} base={base} env={env} seed={seed} supervision={supervision} k={kval} entropy_scoring=true tag='{tag}'
+python idil_train/run_algs.py alg={alg} base={base} env={env} seed={seed} supervision={supervision} k={kval} entropy_scoring=true tag='{tag}' fixed_pi={fixed_pi}
 '''
 
 if __name__ == "__main__":
@@ -25,6 +25,8 @@ if __name__ == "__main__":
                         help="Environment to run the algorithm on")
     parser.add_argument("--custom_tag", type=str, required=False,
                         help="Custom tag for the experiment, appended to the standard ID")
+    parser.add_argument("--fixed_pi", type=bool, default=False, required=False,
+                        help="Whether to use a fixed action policy (expert)")
     
     args = parser.parse_args()
     sweep_k_list = args.sweep_k.split(";")
@@ -33,7 +35,8 @@ if __name__ == "__main__":
 
     print(f"Gotten sweep Ks: {sweep_k_list}")
 
-
+    assert isinstance(args.fixed_pi, bool), "fixed_pi must be a boolean"
+    
     for sweep_k in sweep_k_list:
         for trial_id in range(args.num_trials):
             job_id = ULID()
@@ -56,7 +59,8 @@ if __name__ == "__main__":
                                                     supervision=args.supervision,
                                                     env_type=env_type,
                                                     kval=sweep_k, 
-                                                    tag=tag)
+                                                    tag=tag,
+                                                    fixed_pi=args.fixed_pi)
             
             print(f"\n\nRunning command: {command_str}\n\n")
             subprocess.run(command_str, shell=True)
